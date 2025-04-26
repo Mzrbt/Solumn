51eab192a1b3d42df53c06521ac9355445987612
Mzrbt <jabetmarin07@gmail.com>
2025-04-26 19:59:35 +0200

    Install of SDL2 & implementation on main.c, Window OK

--- code/main.c
+++ code/main.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
-#include "include/board.h"
+#include <SDL2/SDL.h>
 
-int main() {
-    printf("Welcome to Solumn !\n");
-    start_game();
+#include "include/board.h"
+#include "include/game.h"
+
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0){
+	    SDL_Log("Unable to init SDL : %s", SDL_GetError());
+	    return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("Solumn", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
+    
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(3000);
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    //start_game();
     return 0;
 }
