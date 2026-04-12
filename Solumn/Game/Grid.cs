8cd6d3b891a975ce511194bf0040ee4b98932ef8
Mzrbt <jabetmarin07@gmail.com>
2026-04-12 20:36:04 +0200

    [FEAT] Grid class

---
+++ Solumn/Game/Grid.cs
@@ -0,0 +1,46 @@
+using Microsoft.Xna.Framework;
+
+namespace Solumn.Game
+{
+    public class Grid
+    {
+        public const int Columns = 6;
+        public const int Rows = 13;
+
+        private Gem[,] _cells = new Gem[Columns,Rows];
+
+        private Rectangle _rectangle;
+
+        public Grid(Rectangle rectangle)
+        {
+            _rectangle = rectangle;
+            for (int i = 0; i < Columns; i++)
+            {
+                for (int j = 0; j < Rows; j++)
+                {
+                    _cells[i,j] = new Gem(GemColor.Empty);
+                }
+            }
+        }
+
+        public Gem GetGem(int x, int y)
+        {
+            return _cells[x,y];
+        }
+
+        public void SetGem(int x, int y, Gem gem)
+        {
+            _cells[x,y] = gem;
+        }
+
+        public bool IsEmpty(int x, int y)
+        {
+            return _cells[x, y].Color == GemColor.Empty;
+        }
+
+        public bool IsInBounds(int x, int y)
+        {
+            return x >= 0 && x < Columns && y >= 0 && y < Rows;
+        }
+    }
+}
